ff5ac512b67dc036cc61c0bdb69a075949d827a4
Catherine Hardy <83373032+catehardy@users.noreply.github.com>
2022-12-15 14:34:00 +0000

    update comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,17 @@ import * as functions from "firebase-functions";
 import admin = require("firebase-admin");
 admin.initializeApp();
 
-// We can now send a msg to this topic with gcloud CLI, using this syntax:
-// gcloud pubsub topics publish environmental-sensors --message='message body' --attribute='key=value,key=value'
+// TODO:
+// Add some cloud functions to read/write to your firestore database.
+// Then add a function that triggers off a Firestore database change.
+// Create a pubsub listener function as one of the cloud functions in index.ts.
+
+// insertFromPubsub function: listens for messages to environmental-sensors topic, then logs the data and
+// stores it in an object, which it then adds as a collection to Firestore.
+// Note: We don't yet have a function to read/write to the firestore database, but
+// we can send a msg to the topic below with gcloud CLI, using this syntax:
+// gcloud pubsub topics publish environmental-sensors --message='message body' --attribute=
+// 'sensorName=value,temperature=value,humidity=value'
 exports.insertFromPubsub = functions.pubsub.topic("environmental-sensors").onPublish((message, context) => {
   functions.logger.info("The function was triggered at ", context.timestamp);
 
@@ -37,31 +46,9 @@ exports.insertFromPubsub = functions.pubsub.topic("environmental-sensors").onPub
   return admin.firestore().collection("environmentalSensors").add(sensorInfo);
 });
 
-
-// deploy with: firebase deploy --only functions
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// Make a simple function that sends a message to a topic
-// and then that other function does something with it.
-
-// If it's a GET request, return the same message
-// If it's a POST request, get some user data from the request body,
-// modify it, and return it to the user.
-
-// Firebase docs: Call functions via HTTP requests
-// https://firebase.google.com/docs/functions/http-events
-
-// Cloud Functions: moving data from Pubsub to
-// Firebase Cloud Firestore in TypeScript:
-// https://www.youtube.com/watch?v=3Zohd6U6CL8
-
+// helloDan function: listens for Https requests to https://us-central1-cate-test.cloudfunctions.net/helloDan
+// logs the request method;
+// in progress: responds to POST requests with personalized response
 export const helloDan = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello, Dan!", { structuredData: true, numCats: 42 });
   functions.logger.info("Request: ", request.method);
@@ -71,7 +58,3 @@ export const helloDan = functions.https.onRequest((request, response) => {
     response.send("Hello Dan - you have too many cats!");
   }
 });
-
-// Add some cloud functions to read/write to your firestore database.
-// Then add a function that triggers off a Firestore database change.
-// Create a pubsub listener function as one of the cloud functions in index.ts.
